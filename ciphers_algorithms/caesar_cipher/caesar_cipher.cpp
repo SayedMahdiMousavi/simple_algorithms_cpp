fe3012249bd4094d57ec7e19755f73b096bf6269
SayedMahdiMousavi <sajm1950@gmail.com>
2025-04-01 12:50:16 +0330

    modified caesar_cipher.cpp

--- ciphers_algorithms/caesar_cipher/caesar_cipher.cpp
+++ ciphers_algorithms/caesar_cipher/caesar_cipher.cpp
@@ -4,29 +4,45 @@
 using namespace std;
 
 std::string encrypt(std::string value, int key){
-    string alpha = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string result;
-    int index, new_index;
-    for(int i=0; i<value.size(); i++){
+    string alpha, result;
+    int new_index;
 
-        for(int j=0; j<alpha.size(); j++){
-            if(value[i]==alpha[j]){
-                new_index = ((int)alpha[j] + key) % alpha.size();
-                result += alpha[new_index];
-            }
-        }
+    for (char c{' '}; c <= '~'; ++c)
+        alpha += c;
+
+    for(auto ch: value){
+        new_index = (alpha.find(ch) + key) % alpha.size();
+        result += alpha[new_index];
     }
+
     return result;
 }
 
-int main(){
-    std::cout << "Test Code" << std::endl;
-    
-    std::string text = "Hello Python";
-    std::string result;
-    int key = 5;
 
-    result = encrypt(text, key);
-    std::cout << result << std::endl;
+std::string decrypt(std::string value, int key){
+    key *= -1;
+    return encrypt(value, key);
+}
+
+
+int main(){
+    std::string message;
+    int key;
+
+    std::cout << "Enter Message: ";
+    std::getline (std::cin, message);
+    std::cout << " " << std::endl;
+
+    std::cout << "Enter key: ";
+    std::cin >> key;
+    std::cout << " " << std::endl;
+
+    std::string encrypt_result = encrypt(message, key);
+    std::cout << "Encrypt Message: " << encrypt_result << std::endl;
+
+    std::string decrypt_result = decrypt(encrypt_result, key);
+    std::cout << "Decrypt Message: " << decrypt_result << std::endl;
+
+
     return 0;
 }
